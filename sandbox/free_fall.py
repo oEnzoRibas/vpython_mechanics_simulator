2e8d5fb6badb7a9848d9d3fd397ab6117f8c216a
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2026-06-14 22:10:29 -0300

    update free_fall test

--- sandbox/free_fall.py
+++ sandbox/free_fall.py
@@ -1,26 +1,45 @@
 from vpython import *
+import numpy as np
 
-ball = sphere(
-    pos=vector(0,10,0),
-    radius=1,
-    color=color.red
-)
+def main():
+    canvas(
+        title="Free Fall",
+        width=1920,
+        height=1080
+    )
 
-floor = box(
-    pos=vector(0,-10,0),
-    size=vector(10,0.5,10),
-    color=color.green
-)
+    ball = sphere(
+        pos=vector(0,10,0),
+        radius=1,
+        color=color.red
+    )
 
-dt = 0.01
-v = 0
-g = 9.78
 
-while True:
-    rate(60)
+    floor = box(
+        pos=vector(0,-10,0),
+        size=vector(10,0.5,10),
+        color=color.green
+    )
 
-    v -= g*dt
-    ball.pos.y += v * dt
+    dt = np.float64(0.01)
+    v = np.float64(0)
+    g = np.float64(909.78)
 
-    if (ball.pos.y - ball.radius) <= floor.pos.y:
-        v = -v
+
+    while True:
+        rate(60)
+
+        v += -g * dt
+        ball.pos.y += v * dt
+
+        ball_edge = ball.pos.y - ball.radius
+        floor_top_surface = floor.pos.y + floor.size.y/2
+
+        if ball_edge <= floor_top_surface:
+            penetration = floor_top_surface - ball_edge
+            ball.pos.y = floor_top_surface + ball.radius + penetration
+            v = -v
+
+
+if __name__ == "__main__":
+    main()
