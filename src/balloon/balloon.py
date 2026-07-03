8c067cced1d3217e86d8c568fb15861aa2ccc47e
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2026-07-03 16:39:30 -0300

    add cellphone and physics

--- src/balloon/balloon.py
+++ src/balloon/balloon.py
@@ -19,113 +19,161 @@ scene = canvas(
 scene.center = vec(0, 4, 0)
 scene.range = 10
 
-ground = sphere(
-    pos=vec(0, -10.5, 0), 
+ground = box(
+    pos=vec(0, -30.5, 0), 
     size=vec(30, 0.2, 30), 
-    texture=textures.earth
+    texture=textures.granite
     )
 
-envelope = sphere(
-    pos=vec(0, 5, 0),
-    size=vec(4, 4.5, 4),
-    color=color.red,
+# envelope = sphere(
+#     pos=vec(0, 5, 0),
+#     size=vec(4, 4.5, 4),
+#     color=color.red,
+# )
+
+# outershape = [
+#     [0.40, -0.40],
+#     [0.40,  0.40],
+#     [-0.40, 0.40],
+#     [-0.40,-0.40],
+#     [0.40,-0.40]
+# ]
+
+# innershape = [
+#     [0.30,-0.30],
+#     [0.30, 0.30],
+#     [-0.30,0.30],
+#     [-0.30,-0.30],
+#     [0.30,-0.30]
+# ]
+
+# walls = extrusion(
+#     shape=[outershape, innershape],
+#     path=[
+#         vec(0, 0.0, 0),
+#         vec(0, .5, 0)
+#     ],
+#     color=vector(.55, .27, .07)
+# )
+
+# bottom = extrusion(
+#     shape=outershape,
+#     path=[
+#         vec(0, 0.0, 0),
+#         vec(0, 0.1, 0)
+#     ],
+#     color=vector(.55, .27, .07)
+# )
+
+# basket = compound([walls, bottom])
+
+# ropes = []
+
+# for sx in (-1, 1):
+#     for sz in (-1, 1):
+
+#         start = vec(sx * 0.30, 0.50, sz * 0.30)
+#         end   = vec(sx * 0.75, 2.80, sz * 0.75)
+
+#         ropes.append(
+#             cylinder(
+#                 pos=start,
+#                 axis=end - start,
+#                 radius=0.015,
+#                 color=color.white
+#             )
+#         )
+
+# balloon = compound([
+#     envelope,
+#     basket,
+#     *ropes,
+# ])
+
+balloon = sphere(
+    pos=vec(0, 0, 3), 
+    size=vec(2, 2, 2),
+    color=color.green
 )
 
-outershape = [
-    [0.40, -0.40],
-    [0.40,  0.40],
-    [-0.40, 0.40],
-    [-0.40,-0.40],
-    [0.40,-0.40]
-]
-
-innershape = [
-    [0.30,-0.30],
-    [0.30, 0.30],
-    [-0.30,0.30],
-    [-0.30,-0.30],
-    [0.30,-0.30]
-]
-
-walls = extrusion(
-    shape=[outershape, innershape],
-    path=[
-        vec(0, 0.0, 0),
-        vec(0, .5, 0)
-    ],
-    color=vector(.55, .27, .07)
+celular = box(
+    pos=vec(0, 0, 3), 
+    size=vec(2, 2, 2),
+    color=color.red
 )
-
-bottom = extrusion(
-    shape=outershape,
-    path=[
-        vec(0, 0.0, 0),
-        vec(0, 0.1, 0)
-    ],
-    color=vector(.55, .27, .07)
-)
-
-basket = compound([walls, bottom])
-
-ropes = []
-
-for sx in (-1, 1):
-    for sz in (-1, 1):
-
-        start = vec(sx * 0.30, 0.50, sz * 0.30)
-        end   = vec(sx * 0.75, 2.80, sz * 0.75)
-
-        ropes.append(
-            cylinder(
-                pos=start,
-                axis=end - start,
-                radius=0.015,
-                color=color.white
-            )
-        )
-
-balloon = compound([
-    envelope,
-    basket,
-    *ropes,
-])
-
-balloon.pos = vec(0, 0, 0)
 
 t = 0
 dt = 0.01
 
-
-grafico = graph(
+g_pos = graph(
     align="right",
     title="Ball Movement",
     xtitle="Time (s)",
     ytitle="Position",
     width=600,
-    height=250,
+    height=650,
     xmin=0,
-    xmax=10
+    xmax=15,
+    ymin=-10,
+    ymax=20
 )
 
-xmotion = gcurve(graph=grafico, color=color.red)
-ymotion = gcurve(graph=grafico, color=color.blue)
-zmotion = gcurve(graph=grafico, color=vector(0.5,1,.3))
+# g_pos_rel = graph(
+#     align="right",
+#     title="Relative Movement",
+#     xtitle="Time (s)",
+#     ytitle="Position",
+#     width=600,
+#     height=250,
+#     xmin=0,
+#     xmax=10,
+#     ymin=0,
+#     ymax=20
+# )
+
+# g_vel = graph(
+#     align="right",
+#     title="Ball Movement",
+#     xtitle="Time (s)",
+#     ytitle="Position",
+#     width=600,
+#     height=250,
+#     xmin=0,
+#     xmax=10,
+#     ymin=0,
+#     ymax=20
+# )
+
+balloon_graph = gcurve(graph=g_pos, color=color.green)
+celular_graph = gcurve(graph=g_pos, color=color.red)
+# cell_balloon = gcurve(graph=g_pos_rel, color= color.orange)
+
+v0 = 4
+v = v0
+g = np.float64(-1)
 
 while True:
-    rate(100)
+    rate(60)
 
-    x = 9 * np.sin(2*np.pi*t) + 10*np.e**(-0.5*t)*np.cos(2*np.pi*t)
-    y = 9 * np.cos(2*np.pi*t)* 3*np.e**(-0.5*t)
-    z  = 10*np.e**(-0.1*t) * np.sin(t)
+    if celular.pos.y <= -10:
+        continue
+    celular.pos.y = v0 * t + 0.5*g*t**2
+# v = v + g * dt
 
-    balloon.pos = vec(x, y, z)
+    balloon.pos.y = v0*t
 
-    xmotion.plot(t, x)
-    ymotion.plot(t, y)
-    zmotion.plot(t, z)
 
-    if t > 10:
-        grafico.xmin = t - 10
-        grafico.xmax = t
+
+    celular_graph.plot(t, celular.pos.y)
+    balloon_graph.plot(t, balloon.pos.y)
+    # cell_balloon.plot(t, balloon.pos.y-celular.pos.y)
+
+
+
+    # if t > 10:
+    #     g_pos.xmin = t - 10
+    #     g_pos_rel.xmin = t - 10
+    #     g_pos.xmax = t
+    #     g_pos_rel.xmax = t
 
     t += dt
